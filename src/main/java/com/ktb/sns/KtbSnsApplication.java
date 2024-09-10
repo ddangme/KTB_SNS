8ca088e371a7ddfe8bac599dc05d440ce3447c59
pocco <ssilver121@naver.com>
2024-09-10 15:13:03 +0900

    chore: project initial commit

---
+++ src/main/java/com/ktb/sns/KtbSnsApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbSnsApplication.class, args);
+	}
+
+}
